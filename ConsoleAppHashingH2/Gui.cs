e0f970bbf88debfe05364e9911d56f03072fd01b
Mari <uncharted220501@hotmail.com>
2021-01-04 17:27:13 +0100

    uploading code

--- ConsoleAppHashingH2/Gui.cs
+++ ConsoleAppHashingH2/Gui.cs
@@ -35,8 +35,7 @@ namespace ConsoleAppHashingH2
 
         public void SetHashMassage(string hashName, byte[] hashMessageNo1, byte[] hashMessageNo2)
         {
-            Console.WriteLine($"<" +
-                $"\n{hashName} Hashes \n");
+            Console.WriteLine($"\n{hashName} Hashes \n");
             Console.WriteLine("Message 1 hash = " + Convert.ToBase64String(hashMessageNo1));
             Console.WriteLine("Message 2 hash = " + Convert.ToBase64String(hashMessageNo2));
         }
